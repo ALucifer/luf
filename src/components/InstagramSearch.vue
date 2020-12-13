4fab199ea46382c7808610811e4bbd3609172a13
Alexis Coubard <acoubard@macbook-pro-de-alexis.home>
2020-12-13 22:18:44 +0100

    second

--- src/components/InstagramSearch.vue
+++ src/components/InstagramSearch.vue
@@ -2,43 +2,41 @@
   <v-row>
     <InstagramInput @submit="submit" />
     <v-col v-if="medias && medias.length" cols="12">
-      <img
-        v-for="(item, key) in medias"
-        :key="key"
-        :src="item.src.portrait"
-        alt=""
-        width="200"
-        height="200"
-      />
+      <v-row>
+        <v-col 
+          cols="3"
+          v-for="(item, key) in medias"
+          :key="key"
+        >
+          <img
+            :src="item.src.portrait"
+            alt=""
+            width="200"
+            height="200"
+            @click="showImage(item)"
+          />
+        </v-col>
+      </v-row>
     </v-col>
+    <InstagramDialog />
   </v-row>
 </template>
 
 <script>
 import axios from "axios";
 import InstagramInput from "./InstagramInput.vue";
+import InstagramDialog from "./InstagramDialog.vue"
 
 export default {
   components: {
-    InstagramInput,
+    InstagramInput, InstagramDialog
   },
   data() {
     return {
       medias: [],
+      selectedMedia: null,
       word: "",
     };
-  },
-  created: function() {
-    /*axios
-      .get("https://api.pexels.com/v1/search?query=nature&per_page=5", {
-        headers: {
-          Authorization:
-            "563492ad6f917000010000016760c2e0166844f7890386c1fd944362",
-        },
-      })
-      .then((res) => {
-        this.medias = res.data.photos;
-      });*/
   },
   methods: {
     submit(value) {
@@ -53,6 +51,9 @@ export default {
           this.medias = res.data.photos;
         });
     },
+    showImage(media) {
+      this.selectedMedia = media;
+    }
   },
 };
 </script>
